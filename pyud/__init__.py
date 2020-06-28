d17c47729e610800a4eee5fb1537378e102813dd
William Lee <wlee753159@gmail.com>
2020-06-28 11:16:32 +0100

    Add module files

---
+++ pyud/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+pyud
+~~~~
+
+A simple wrapper for the Urban Dictionary API
+
+:copyright: 2020 William Lee
+:license: GNU GPL v3.0, see LICENSE
+"""
+
+from .definition import Definition
---
+++ pyud/definition.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+pyud.definition
+~~~~~~~~~~~~~~~
+
+:copyright: 2020 William Lee
+:license: GNU GPL v3.0, see LICENSE
+"""
+
+
+class Definition:
+    pass
