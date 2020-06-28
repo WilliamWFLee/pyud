8c547d4ff4d45ee9bc028c893ce2bb812a469a40
William Lee <wlee753159@gmail.com>
2020-06-28 20:23:21 +0100

    Remove parameter lists from client.py

--- pyud/client.py
+++ pyud/client.py
@@ -21,17 +21,17 @@ RANDOM_URL = BASE_URL + "random"
 
 
 class ClientBase:
-    """Base class for the Client and AsyncClient"""
+    """
+    Base class for the Client and AsyncClient
+    """
     def _from_json(
         self, data: Union[str, bytes, bytearray]
     ) -> Optional[List[Definition]]:
-        """Returns a list of Definitions from JSON
+        """
+        Returns a list of Definitions from JSON
 
         The format of the JSON is a single array of definition objects
         under the key 'list' in the JSON document
-
-        :param data: The definitions in JSON format
-        :type data: Union[str, bytes, bytearray]
         """
 
         try:
@@ -57,41 +57,33 @@ class ClientBase:
 
 
 class Client(ClientBase):
-    """Synchronous client for the Urban Dictionary API"""
+    """
+    Synchronous client for the Urban Dictionary API
+    """
     def _fetch_definitions(self, url: str) -> Optional[List[Definition]]:
-        """Fetch definitions from the API url given"""
+        """
+        Fetch definitions from the API url given
+        """
         with request.urlopen(url) as response:
             return self._from_json(response.read().decode('utf-8'))
 
     def define(self, term: str) -> Optional[List[Definition]]:
-        """Finds definitions for a given term
-
-        :param url: The API URL
-        :type url: str
-        :return: A list of Definitions or `None` for no definitions
-        :rtype: Optional[List[Definition]]
+        """
+        Finds definitions for a given term
         """
         return self._fetch_definitions(DEFINE_BY_TERM_URL.format(term))
 
     def from_id(self, defid: int) -> Optional[Definition]:
-        """Finds a definition by ID
-
-        :param defid: The ID of the definition
-        :type defid: int
-        :return: The definition or `None` if no definition is found
-        :rtype: Optional[List[Definition]]
+        """
+        Finds a definition by ID
         """
         definitions = self._fetch_definitions(DEFINE_BY_ID_URL.format(defid))
 
         return definitions[0] if definitions else None
 
     def random(self, *, limit: int = 10) -> List[Definition]:
-        """Returns a random list of definitions
-
-        :param limit: The number of definitions to return. Defaults to 10, must be at most 10
-        :type limit: int, optional
-        :return: A list of definitions
-        :rtype: List[Definition]
+        """
+        Returns a random list of definitions
         """
         definitions = self._fetch_definitions(RANDOM_URL)
 
@@ -99,7 +91,9 @@ class Client(ClientBase):
 
 
 class AsyncClient(ClientBase):
-    """Asynchronous client for the Urban Dictionary API"""
+    """
+    Asynchronous client for the Urban Dictionary API
+    """
     async def _fetch_definitions(self, url: str) -> Optional[List[Definition]]:
         """
         Fetch definitions from the API url given
