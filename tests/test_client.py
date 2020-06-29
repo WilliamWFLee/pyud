d5e91f2c7ccef3c85165941cf0d2b2e7c2a69c73
William Lee <wlee753159@gmail.com>
2020-06-29 13:35:09 +0100

    Add synchronous client tests

---
+++ tests/test_client.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+import pytest
+
+import pyud
+
+
+@pytest.fixture
+def client():
+    return pyud.Client()
+
+@pytest.mark.incremental
+class TestClient:
+    def test_client(self, client):
+        assert client
+
+    def test_client_define(self, client):
+        assert client.define('hello')
+        assert client.define('skadhfujahkduikfbresfvkayjfsrvued') is None
+
+    def test_client_from_id(self, client):
+        assert client.from_id(2452556).word == "Ping"
+        assert client.from_id(1) is None
+
+    def test_client_random(self, client):
+        definitions = client.random()
+        assert definitions
+        assert len(definitions) == 10
+
+    def test_client_random_limit(self, client):
+        assert len(client.random(limit=3)) == 3
+        assert len(client.random(limit=10)) == 10
+        assert len(client.random(limit=43)) == 10
