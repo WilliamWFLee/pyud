f3e578cbe6d5879774df5673760beedd6672fde3
William Lee <wlee753159@gmail.com>
2020-06-29 10:39:59 +0100

    Add incomplete data definition test

--- tests/test_definition.py
+++ tests/test_definition.py
@@ -1,18 +1,30 @@
 # -*- coding: utf-8 -*-
+import pytest
+
 import pyud
+
+DATA = {
+    "defid": 1,
+    "word": "hello",
+    "definition": "a very rude word",
+    "author": "me",
+    "thumbs_up": 13423,
+    "thumbs_down": 43,
+    "example": "hello [Karen]",
+    "permalink": "http://hello.urbanup.com/14231",
+    "sound_urls": [],
+    "written_on": "2020-06-29T00:00:00.000Z",
+}
 
 
 def test_complete_definition():
-    data = {
-        "defid": 1,
-        "word": "hello",
-        "definition": "a very rude word",
-        "author": "me",
-        "thumbs_up": 13423,
-        "thumbs_down": 43,
-        "example": "hello [Karen]",
-        "permalink": "http://hello.urbanup.com/14231",
-        "sound_urls": [],
-        "written_on": "2020-06-29T00:00:00.000Z",
-    }
-    assert pyud.Definition(**data) is not None
+    assert pyud.Definition(**DATA) is not None
+
+
+def test_incomplete_definition():
+    # No defid
+    no_defid_data = DATA.copy()
+    del no_defid_data["defid"]
+
+    with pytest.raises(TypeError):
+        definition = pyud.Definition(**no_defid_data)
