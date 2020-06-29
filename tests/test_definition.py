cfa6c69357a13c6e1996a8c8baac3b6edfaa41ee
William Lee <wlee753159@gmail.com>
2020-06-29 11:01:23 +0100

    Add tests for incorrect date format

--- tests/test_definition.py
+++ tests/test_definition.py
@@ -16,6 +16,15 @@ DATA = {
     "written_on": "2020-06-29T00:00:00.000Z",
 }
 
+INCORRECT_DATES = (
+    "2020-06-29T00:00:00.000",  # Missing 'Z'
+    "2020-06-29T00:00:00:000Z",  # : instead of . separating ms from s
+    "2020-06-29T00:00:00Z",  # No millis
+    "2020-06-29 00:00:00.000Z",  # Space instead of T
+    "2020/06/29T00:00:00.000Z",  # Incorrect delimters for date
+    "2020-06-29",  # Date-only
+)
+
 
 def test_complete_definition():
     assert pyud.Definition(**DATA) is not None
@@ -28,3 +37,12 @@ def test_incomplete_definition():
 
         with pytest.raises(TypeError):
             definition = pyud.Definition(**incomplete_data)
+
+
+def test_incorrect_date_format():
+    for date in INCORRECT_DATES:
+        data = DATA.copy()
+        data["written_on"] = date
+
+        with pytest.raises(ValueError):
+            definition = pyud.Definition(**data)
