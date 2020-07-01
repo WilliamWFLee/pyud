164137bc7f2639a1e79479b91e038eea1ddc5d13
William Lee <wlee753159@gmail.com>
2020-07-01 12:02:49 +0100

    Update license classfier

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ setup(
     classifiers=[
         "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Developers",
-        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
         "Natural Language :: English",
         "Operating System :: OS Independent",
         "Programming Language :: Python :: 3 :: Only",
