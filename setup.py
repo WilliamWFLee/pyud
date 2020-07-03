79149bde39b943c46bb1e8379e5478afd843a789
William Lee <wlee753159@gmail.com>
2020-07-03 16:44:38 +0100

    Add Python 3.5 package classifier to setup.py

--- setup.py
+++ setup.py
@@ -41,6 +41,7 @@ setup(
         "Natural Language :: English",
         "Operating System :: OS Independent",
         "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.5"
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
