014f8795e2f61a9c85c5ad326ad44a97e207b255
William Lee <wlee753159@gmail.com>
2020-07-07 12:18:14 +0100

    Add sync reference tests

--- tests/test_reference.py
+++ tests/test_reference.py
@@ -4,6 +4,18 @@ import pytest
 import pyud
 
 
+@pytest.fixture
+def reference():
+    ud = pyud.Client()
+    return pyud.Reference(ud, "hello")
+
+
 @pytest.mark.incremental
 class TestReference:
-    pass
+    def test_reference_word(self, reference):
+        assert reference.word == "hello"
+
+    def test_reference_define(self, reference):
+        definitions = reference.define()
+        for definition in definitions:
+            assert definition.word.lower() == "hello"
