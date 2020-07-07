bf4bd2c552739ce9baa370368cfb45277143e9c9
William Lee <wlee753159@gmail.com>
2020-07-07 10:43:16 +0100

    Add AsyncReference tests

--- tests/test_async_reference.py
+++ tests/test_async_reference.py
@@ -4,6 +4,20 @@ import pytest
 import pyud
 
 
+@pytest.fixture
+def reference():
+    ud = pyud.AsyncClient()
+    return pyud.AsyncReference(ud, "hello")
+
+
 @pytest.mark.incremental
 class TestAsyncReference:
-    pass
+    @pytest.mark.asyncio
+    async def test_reference_word(self, reference):
+        assert reference.word == "hello"
+
+    @pytest.mark.asyncio
+    async def test_reference_define(self, reference):
+        definitions = await reference.define()
+        for definition in definitions:
+            assert definition.word.lower() == "hello"
