c9e78ae7f8c68315a543bceeba13bf39ac24c330
William Lee <wlee753159@gmail.com>
2020-07-08 20:47:10 +0100

    Implementation define subcommand

--- pyud/__main__.py
+++ pyud/__main__.py
@@ -20,12 +20,53 @@ along with pyud.  If not, see <https://www.gnu.org/licenses/>.
 """
 
 import argparse
+import shutil
+from typing import List
 
 import pyud
 
 
+def show_results(definitions: List[pyud.Definition]):
+    showing = True
+    index = 0
+    while showing:
+        definition = definitions[index]
+
+        cols = shutil.get_terminal_size().columns
+        heading_template = "{{:-^{}}}".format(cols)
+
+        page_content = (
+            heading_template.format(
+                "Page {} of {}".format(index + 1, len(definitions))
+            ),
+            heading_template.format(
+                "Showing definition for {!r}".format(definition.word)
+            ),
+            heading_template.format('Definition'),
+            definition.definition,
+            heading_template.format('Example'),
+            definition.example,
+        )
+        page = "\n".join(page_content) + "\n"
+        print(page)
+
+        try:
+            command = input("Options - (N)ext, (P)revious, (Q)uit: ")
+        except EOFError:
+            print()
+            break
+        if command.upper() == "Q":
+            break
+        elif command.upper() == "N":
+            index = (index + 1) % len(definitions)
+        elif command.upper() == "P":
+            index = (index - 1) % len(definitions)
+
+
 def define(parser, args):
-    pass
+    ud = pyud.Client()
+    definitions = ud.define(args.term)
+    show_results(definitions)
 
 
 def add_define_args(subparser):
@@ -45,7 +86,7 @@ def parse_args():
     parser.add_argument(
         '--version',
         action='version',
-        version="pyud {}".format(pyud.__version__)
+        version="pyud {}".format(pyud.__version__),
     )
 
     subparser = parser.add_subparsers(metavar='subcommand')
